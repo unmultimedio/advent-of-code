88f0a0e034f6fa0fbcd6d6e3bcbff72f97bec68e
Julian R Figueroa <jfigueroa@twilio.com>
2020-12-15 22:04:03 -0500

    Day 8, part 2

--- 2020/day-08/main.go
+++ 2020/day-08/main.go
@@ -11,47 +11,79 @@ import (
 var accumulator int
 var orders []string
 var executed map[int]struct{}
+var changeAttempts map[int]struct{}
+var alreadyFlipped bool
 
 func main() {
 	orders, _ = util.ReadLines("./input")
-	executed = make(map[int]struct{})
-	executeOrder(0)
+	changeAttempts = make(map[int]struct{})
+
+	for {
+		fmt.Println("--- starting over")
+		accumulator = 0
+		alreadyFlipped = false
+		executed = make(map[int]struct{})
+		keepTrying := executeOrder(0)
+		if !keepTrying {
+			break
+		}
+	}
 }
 
-func executeOrder(idx int) {
-	if idx >= len(orders) {
-		fmt.Printf("index %d is out of bounds\n", idx)
-		return
+func executeOrder(idx int) (keepTrying bool) {
+	if idx == len(orders) {
+		fmt.Printf("arrived to the final instruction + 1! index: %d. current accumulator: %d\n", idx, accumulator)
+		return false
+	}
+
+	if idx < 0 || idx > len(orders) {
+		fmt.Printf("index %d is out of bounds. current accumulator: %d\n", idx, accumulator)
+		return true
 	}
 
 	_, alreadyExecuted := executed[idx]
 	if alreadyExecuted {
 		fmt.Printf("order %d was already executed. current accumulator: %d\n", idx, accumulator)
-		return
+		return true
 	}
 
 	executed[idx] = struct{}{}
 	order := orders[idx]
 	parts := strings.Split(order, " ")
 	if len(parts) != 2 {
-		fmt.Printf("wrong order, not two parts: %q\n", order)
-		return
+		panic(fmt.Sprintf("wrong order, not two parts: %q\n", order))
 	}
 
 	argument, err := strconv.Atoi(parts[1])
 	if err != nil {
-		fmt.Printf("wrong order, bad argument: %q\n", parts[1])
-		return
+		panic(fmt.Sprintf("wrong order, bad argument: %q\n", parts[1]))
 	}
+
 	switch parts[0] {
 	case "acc":
 		accumulator += argument
-		executeOrder(idx + 1)
+		return executeOrder(idx + 1)
 	case "jmp":
-		executeOrder(idx + argument)
+		_, alreadyTried := changeAttempts[idx]
+		if alreadyTried || alreadyFlipped {
+			return executeOrder(idx + argument)
+		}
+		// execute nop
+		alreadyFlipped = true
+		changeAttempts[idx] = struct{}{}
+		fmt.Printf("flipping %d: %q\n", idx, order)
+		return executeOrder(idx + 1)
 	case "nop":
-		executeOrder(idx + 1)
-	default:
-		fmt.Printf("wrong order, unknown operation: %q\n", parts[0])
+		_, alreadyTried := changeAttempts[idx]
+		if alreadyTried || alreadyFlipped {
+			return executeOrder(idx + 1)
+		}
+		// execute jmp
+		alreadyFlipped = true
+		changeAttempts[idx] = struct{}{}
+		fmt.Printf("flipping %d: %q\n", idx, order)
+		return executeOrder(idx + argument)
 	}
+
+	panic(fmt.Sprintf("wrong order %q, unknown operation: %q\n", order, parts[0]))
 }
