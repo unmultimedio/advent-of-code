42770493b3038566cf27bf29f77c9edb2840ca0d
Julian R Figueroa <jfigueroa@twilio.com>
2020-12-15 20:02:45 -0500

    Day 6, part 2

--- 2020/day-06/main.go
+++ 2020/day-06/main.go
@@ -18,14 +18,30 @@ func main() {
 	fmt.Printf("total yes: %d\n", totalYeses)
 }
 
+// you need to identify the questions to which everyone answered "yes"!, meaning
+// I can iterate over the first one only.
 func countPositiveAnswers(groupAswers string) int {
+	peopleAnswers := strings.Split(groupAswers, "\n")
+
 	yeses := make(map[int]struct{})
-	for _, s := range groupAswers {
+	// only first person
+	for _, s := range peopleAnswers[0] {
 		ascii := int(s)
 		if ascii < 97 || ascii > 122 { // a-z
 			continue
 		}
 		yeses[ascii] = struct{}{}
+
+		// check if others have it as well, or we should remove.
+		for idx, others := range peopleAnswers {
+			if idx == 0 {
+				continue
+			}
+
+			if !strings.Contains(others, string(s)) {
+				delete(yeses, ascii)
+			}
+		}
 	}
 
 	return len(yeses)
