7b28c4be66b7520907d68be0b7ad720d9919df1c
Julian R Figueroa <jfigueroa@twilio.com>
2020-12-16 00:34:51 -0500

    Day 10, part 2

--- 2020/day-10/main.go
+++ 2020/day-10/main.go
@@ -8,9 +8,7 @@ import (
 )
 
 var numbers []int
-var possibleArrangements int
-
-// var m sync.Mutex
+var pathsFromIndex map[int]int
 
 func main() {
 	numbersRaw, _ := util.ReadLines("./input-sorted")
@@ -25,27 +23,36 @@ func main() {
 	}
 	numbers = append(numbers, numbers[len(numbers)-1]+3) // device
 
-	// differences := make(map[int]int)
-	// for i := 1; i < len(numbers); i++ {
-	// 	differences[numbers[i]-numbers[i-1]]++
-	// }
-	// fmt.Printf("differences in joltage: %v\n", differences)
-	// fmt.Printf("1-diff * 3-diff: %d\n", differences[1]*differences[3])
+	differences := make(map[int]int)
+	for i := 1; i < len(numbers); i++ {
+		differences[numbers[i]-numbers[i-1]]++
+	}
+	fmt.Printf("differences in joltage: %v\n", differences)
+	fmt.Printf("1-diff * 3-diff: %d\n", differences[1]*differences[3])
 
-	iterateFrom(0)
-	fmt.Printf("possible arrangements: %d", possibleArrangements)
+	pathsFromIndex = make(map[int]int)
+	combs := pathsFrom(0)
+	fmt.Printf("possible combinations: %d\n", combs)
 }
 
-func iterateFrom(idx int) {
+func pathsFrom(idx int) int {
 	if idx == len(numbers)-1 {
-		possibleArrangements++
-		return
+		return 1
 	}
 
-	for i := idx + 1; i < len(numbers); i++ {
-		if numbers[i]-numbers[idx] > 3 {
+	paths, alreadyCalculated := pathsFromIndex[idx]
+	if alreadyCalculated {
+		return paths
+	}
+
+	for nxt := idx + 1; nxt < len(numbers); nxt++ {
+		if numbers[nxt]-numbers[idx] <= 3 {
+			paths += pathsFrom(nxt)
+		} else {
 			break
 		}
-		iterateFrom(i)
 	}
+	pathsFromIndex[idx] = paths
+
+	return paths
 }
