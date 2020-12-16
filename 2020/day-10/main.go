23f44c8e234261b80e541aac527e91f9074d38e2
Julian R Figueroa <jfigueroa@twilio.com>
2020-12-15 23:23:47 -0500

    Day 1, part 2, Attempt 1

--- 2020/day-10/main.go
+++ 2020/day-10/main.go
@@ -7,10 +7,14 @@ import (
 	"github.com/unmultimedio/adventofcode/util"
 )
 
+var numbers []int
+var possibleArrangements int
+
+// var m sync.Mutex
+
 func main() {
 	numbersRaw, _ := util.ReadLines("./input-sorted")
 
-	var numbers []int
 	numbers = append(numbers, 0) // port
 	for _, nRaw := range numbersRaw {
 		n, err := strconv.Atoi(nRaw)
@@ -21,10 +25,27 @@ func main() {
 	}
 	numbers = append(numbers, numbers[len(numbers)-1]+3) // device
 
-	differences := make(map[int]int)
-	for i := 1; i < len(numbers); i++ {
-		differences[numbers[i]-numbers[i-1]]++
+	// differences := make(map[int]int)
+	// for i := 1; i < len(numbers); i++ {
+	// 	differences[numbers[i]-numbers[i-1]]++
+	// }
+	// fmt.Printf("differences in joltage: %v\n", differences)
+	// fmt.Printf("1-diff * 3-diff: %d\n", differences[1]*differences[3])
+
+	iterateFrom(0)
+	fmt.Printf("possible arrangements: %d", possibleArrangements)
+}
+
+func iterateFrom(idx int) {
+	if idx == len(numbers)-1 {
+		possibleArrangements++
+		return
 	}
-	fmt.Printf("differences in joltage: %v\n", differences)
-	fmt.Printf("1-diff * 3-diff: %d\n", differences[1]*differences[3])
+
+	for i := idx + 1; i < len(numbers); i++ {
+		if numbers[i]-numbers[idx] > 3 {
+			break
+		}
+		iterateFrom(i)
+	}
 }
